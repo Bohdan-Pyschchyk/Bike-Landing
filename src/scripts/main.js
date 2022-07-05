d2a6db55251b48ef0372462d8b07a1c15d1b3603
Bogdan Pyshchyk <bohdan.pyshchyk@gmail.com>
2022-07-05 15:06:50 +0300

    Clone Bike-landing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,33 @@
 'use strict';
+
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+Swiper.use([ Navigation, Pagination ]);
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const forma = document.getElementById('form');
+
+forma.addEventListener('submit', function(event) {
+  event.preventDefault();
+  forma.reset();
+});
